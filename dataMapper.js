f4308d93b0ef7f3a1f27e3fd61b2af1273682423
Cüneyt <fcuneyt@users.noreply.github.com>
2016-06-03 11:03:06 +0300

    Create dataMapper.js

---
+++ dataMapper.js
@@ -0,0 +1,61 @@
+var dataMapper = {};
+(function () {
+	var self = this;
+	var listFunction = null;
+	var primitiveFunction = null;
+	var getViewModelAttribute = function (viewModelKeys, key) {
+		var result = null;
+		for (var i = 0; i < viewModelKeys.length; i++) {
+			var viewModelAttribute = viewModelKeys[i];
+			if (viewModelAttribute === key) {
+				result = viewModelAttribute;
+				break;
+			}
+		}
+		if (result === null) {
+			result = key;
+		}
+		return result;
+	};
+	var getData = function (value) {
+		var result;
+		if (Object.prototype.toString.call(value) === "[object Array]") {
+			var array = [];
+			for (var j = 0; j < value.length; j++) {
+				var item = value[j];
+				array.push(getData(item));
+			}
+			result = listFunction
+				? listFunction(array)
+				: array;
+		} else if (typeof (value) == "object") {
+			var objectKeys = Object.keys(value);
+			result = {};
+			for (var i = 0; i < objectKeys.length; i++) {
+				var objectItem = objectKeys[i];
+				result[objectItem] = getData(value[objectItem]);
+			}
+		} else {
+			result = primitiveFunction
+				? primitiveFunction(value)
+				: value;
+		}
+		return result;
+	};
+	var setValue = function (instance, attribute, value) {
+		instance[attribute] = getData(value);
+	};
+	self.mapResponse = function (response, viewModel, listFunc, primitiveFunc) {
+		listFunction = listFunc;
+		primitiveFunction = primitiveFunc;
+		var responseKeys = Object.keys(response);
+		var viewModelKeys = Object.keys(viewModel);
+		for (var i = 0; i < responseKeys.length; i++) {
+			var key = responseKeys[i];
+
+			var viewModelAttribute = getViewModelAttribute(viewModelKeys, key);
+
+			setValue(viewModel, viewModelAttribute, response[key]);
+		}
+	};
+}).apply((dataMapper));
